ccaee0e1fbf2f81e4d603001637afaf100c43732
Prince Kumar <kumarprince1407@gmail.com>
2023-07-17 11:54:45 +0530

    Added initial commit

---
+++ FindMaximum/src/main/java/com/bridgelabz/find_maximum/FindMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.find_maximum;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("Find maximum among the given numbers.");
+    }
+}
