e564625ca0d92e3a5ced2dd3365687007ee616a6
Prince Kumar <kumarprince1407@gmail.com>
2023-07-17 12:58:45 +0530

    Added Use Case 2

--- FindMaximum/src/main/java/com/bridgelabz/find_maximum/FindMaximum.java
+++ FindMaximum/src/main/java/com/bridgelabz/find_maximum/FindMaximum.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.find_maximum;
 
 public class FindMaximum {
-    public static int findMaximum(int a,int b, int c){
+    public static int findMaximumInteger(int a,int b, int c){
         int max=0;
         if(a>=b && a>=c){
             max=a;
@@ -12,6 +12,20 @@ public class FindMaximum {
         }
         return max;
     }
+    public static float findMaximumFloat(float a, float b, float c){
+        float max =0;
+        if(a>=b && a>=c){
+            max=a;
+        } else if (a<=b && b>c) {
+            max=b;
+        }else {
+            max=c;
+        }
+        return max;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Find maximum among the given numbers.");
     }
